7abb708c02bc594f2fa9361e8a66156e138b16ab
Quai Wentt <quaiw@zillowgroup.com>
2022-04-17 10:56:16 -0400

    make some slight tweaks

--- src/game.py
+++ src/game.py
@@ -2,6 +2,7 @@ from src.player import Player
 from src.board import Board
 from src.computer import Computer
 from src.enums.game_modes import GameMode
+from src.enums.moves import Moves
 from src.enums.valid_positions import VALID_POSITIONS
 from src.constants.constants import GAME_DIFFICULTY_LEVELS, GAME_MODES
 
@@ -13,11 +14,10 @@ class Game:
         self.gameMode = gameMode if gameMode else GameMode(Game.getInput("Choose your game mode\n1) 1 player\n2) 2 player\n", GAME_MODES))
         difficulty = None if self.isTwoPlayerGame() else Game.getInput(
             "Choose your difficulty\n1) easy\n2) medium\n3) hard\n", GAME_DIFFICULTY_LEVELS)
-        self.player1 = Player(Game.getInput("What is player 1's name? "), "X")
-        self.player2 = Player(Game.getInput("What is player 2's name? "), "O") if self.isTwoPlayerGame() else Player('Computer', "O")
+        self.player1 = Player(Game.getInput("What is player 1's name? "), Moves.X.value)
+        self.player2 = Player(Game.getInput("What is player 2's name? "), Moves.O.value) if self.isTwoPlayerGame() else Player('Computer', Moves.O.value)
         self.board = Board()
         self.isPlayer1Turn = True
-        self.isComplete = False
         self.computer = Computer(difficulty) if not self.isTwoPlayerGame() else None
 
     def say(message):
@@ -55,9 +55,6 @@ class Game:
 
     def updateTurn(self):
         self.isPlayer1Turn = not self.isPlayer1Turn
-
-    def isGameOver(self):
-        return self.isComplete
 
     def makeMove(self):
         player = self.player1 if self.isPlayer1Turn else self.player2
--- tictactoe.py
+++ tictactoe.py
@@ -4,19 +4,21 @@ from src.enums.game_modes import GameMode
 Game.say("Welcome to Tic Tac Toe!")
 # game = Game() when one player is supported
 game = Game(GameMode.TWO_PLAYER) # only two player supported at the moment
-Game.say("Player 1 is X and will go first")
-Game.say("Player 2 is O and will go second")
+Game.say(f'{game.player1.name} is X and will go first')
+Game.say(f'{game.player2.name} is O and will go second')
 
 winner = None
 while not winner and not game.boardIsFull():
     game.drawBoard()
-    Game.say('=======')
+    Game.say('=====')
     game.drawValidMoves()
     game.makeMove()
     winner = game.getWinner()
     if winner:
+        game.drawBoard()
         Game.say(f'{winner.name} is the winner!!!')
     elif game.boardIsFull():
+        game.drawBoard()
         Game.say("It was a draw!")
 
 
